372930f084c4e705f738d2777118d40042f6a523
Kvsvicentini <kaua.vicentini@gmail.com>
2025-05-05 15:01:56 -0300

    Atualizando códigos

--- List_de_Compras.py
+++ List_de_Compras.py
@@ -1,3 +1,67 @@
+def add_shopping_list(shopping):
+    buy = input("Digite um produto para a lista de compras: ")
+    shopping.append(buy)
+    print("Produto adicionado")
+
+def list_shopping_list(shopping):
+    if shopping:
+        print("\nProdutos na lista de compras:")
+        for i, buy in enumerate(shopping, 1):
+            print(f"{i}. {buy}")
+    else:
+        print("A lista de compras está vazia")
+
+def remove_shopping_list(shopping):
+    if not shopping:
+        print("Não há produtos na lista de compras para remover")
+        return
+    list_shopping_list(shopping)
+    try:
+        num = int(input("Digite o número do produto a ser excluído: "))
+        if 1 <= num <= len(shopping):
+            remove_buy = shopping.pop(num - 1)
+            print(f"Produto: '{remove_buy}' removido com sucesso.")
+        else:
+            print("Número do produto inválido")
+    except ValueError:
+        print("Digite um número válido.")
+
+def menu():
+    print("\nEscolha uma opção")
+    print("1-Adicionar um produto a lista de compras")
+    print("2-Listar os produtos da lista de compras")
+    print("3-Remover um produto da lista de compras")
+    print("4-Sair do sistema")
+
+def main():
+    shopping = []
+    print("\nVamos criar uma lista de compras.")
+
+    while True:
+        menu()
+
+        try: 
+            option = int(input("\nDigite a opção que deseja: "))
+        except ValueError:
+            print("Digite um número válido.")
+            continue
+        match option:
+            case 1:
+                add_shopping_list(shopping)
+            case 2:
+                list_shopping_list(shopping)
+            case 3:
+                remove_shopping_list(shopping)
+            case 4:
+                print("Saindo do sistema.")
+                break
+            case _:
+                print("Opção inválida. Tente novamente.")
+
+if __name__ == "__main__":
+    main()
+
+'''''
 list = []
 print("Vamos criar sua lista de compras, caso já tenha terminado, digite fim")
 while True:
@@ -6,3 +70,4 @@ while True:
         break
     list.append(item)
 print(f"Sua lista de compras: {list}")
+'''''
--- List_tasks.py
+++ List_tasks.py
@@ -1,6 +1,67 @@
-list_tasks = []
-print("\nVamos criar uma lista de tarefas.")
+def add_tasks(tasks):
+    task = input("Digite uma tarefa: ")
+    tasks.append(task)
+    print("Tarefa adicionada")
 
+def list_tasks(tasks):
+    if tasks:
+        print("\nTarefas cadastradas:")
+        for i, task in enumerate(tasks, 1):
+            print(f"{i}. {task}")
+    else:
+        print("A lista está vazia")
+
+def remove_tasks(tasks):
+    if not tasks:
+        print("Não há tarefas para remover")
+        return
+    list_tasks(tasks)
+    try:
+        num = int(input("Digite o número da tarefa a ser excluído: "))
+        if 1 <= num <= len(tasks):
+            remove_task = tasks.pop(num - 1)
+            print(f"Tarefa {remove_task} removida com sucesso.")
+        else:
+            print("Número da tarefa inválido")
+    except ValueError:
+        print("Digite um número válido.")
+
+def menu():
+    print("\nEscolha uma opção")
+    print("1-Adicionar uma tarefa")
+    print("2-Listar as tarefas")
+    print("3-Remover uma tarefa")
+    print("4-Sair do sistema")
+
+def main():
+    tasks = []
+    print("\nVamos criar uma lista de tarefas.")
+
+    while True:
+        menu()
+
+        try: 
+            option = int(input("\nDigite a opção que deseja: "))
+        except ValueError:
+            print("Digite um número válido.")
+            continue
+        match option:
+            case 1:
+                add_tasks(tasks)
+            case 2:
+                list_tasks(tasks)
+            case 3:
+                remove_tasks(tasks)
+            case 4:
+                print("Saindo do sistema.")
+                break
+            case _:
+                print("Opção inválida. Tente novamente.")
+
+if __name__ == "__main__":
+    main()
+
+    '''''
 while True:
     try:
         option = int(input("\nDigite uma das opções:\n1-Adicionar\n2-Listar\n3-Remover\n4-Sair\n"))
@@ -42,3 +103,4 @@ while True:
             print("\nA lista está vazia, não há tarefas para remover.")            
     else:
         print("Opção inválida>")
+'''''
