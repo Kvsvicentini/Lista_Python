b81d8f03c0aeff4767de8d1e5b6e32d1c2114066
Kvsvicentini <kaua.vicentini@gmail.com>
2025-05-29 17:53:06 -0300

    x

--- Python_basico/LI-01_EX-01.py
+++ Python_basico/LI-01_EX-01.py
@@ -1,5 +1,5 @@
 print("---------------------------------------------------------------------------------------------")
-print("Vamos ler um número, realizar uma operação matemáticas e mostrar o resultado")
+print("Vamos ler um número, realizar uma operação matemática e mostrar o resultado")
 num_1 = float(input("Digite o número a ser lido: "))
 calc = num_1 + 5
 print(f"O resultado da operação matemática é {calc}")
