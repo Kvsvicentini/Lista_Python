32293946a9a6069af1adbc1da7522018057393b8
Kvsvicentini <kaua.vicentini@gmail.com>
2025-06-10 21:58:50 -0300

    Mudei coisas

--- Adivinha_Num.py
+++ Adivinha_Num.py
@@ -1,7 +1,12 @@
 import random
 num_secret = random.randint(1, 10)
-attempt = int(input("Tente adivinhar o número sorteado de 1 a 10: "))
-if attempt == num_secret:
-    print(f"Parabéns! Você acertou o número, o número era {num_secret}")
-else:
-    print(f"Errou! O número era {num_secret}.")
+cont = 0
+print("Tente adivinhar o número sorteado de 1 a 10")
+while True:
+    attempt = int(input("Digite um número: "))
+    cont += 1
+    if attempt == num_secret:
+        print(f"Parabéns! Você acertou o número depois de {cont} vezes, o número era {num_secret}")
+        break
+    else:
+        print(f"Errou! Tente novamente.")
--- Generate_multiplication_table.py
+++ Generate_multiplication_table.py
@@ -1,4 +1,14 @@
 num = int(input("Digite um número e criaremos uma tabuada com o número digitado: "))
-print(f"A tabuada do {num}:")
-for i in range(1, 11,):
-    print(f"{num} x {i} = {num * i}")
+operation = input("Digite a operação que deseja utilizar na tabuada: +, -, *, / \n")
+if (operation == "*"):
+    print(f"A tabuada do {num}:")
+    for i in range(1, 11,):
+        print(f"{num} x {i} = {num * i}")
+elif (operation == "+"):
+    print(f"A tabuada do {num}:")
+    for i in range(1, 11,):
+        print(f"{num} + {i} = {num + i}")
+elif (operation == "-"):
+    print(f"A tabuada do {num}:")
+    for i in range(1, 11,):
+        print(f"{num} - {i} = {num - i}")
